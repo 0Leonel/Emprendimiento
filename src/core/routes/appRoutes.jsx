cf5d54167d5d2f855ea81e1fffc6272e038c0e42
0Leonel <leonel20112010@gmail.com>
2024-03-07 12:18:53 -0300

    2do

--- src/core/routes/appRoutes.jsx
+++ src/core/routes/appRoutes.jsx
@@ -67,7 +67,7 @@ export const appRoutes = createBrowserRouter([
         },
         {
             path: "contacto",
-            element: <ViewCategory category="4"/>,
+            element: <ViewCategory category="5"/>,
         }
   
        
